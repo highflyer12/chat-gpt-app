672fac20c28799d817d90065fc64e967fda1a22b
hf <hf@hfmini.local>
2024-12-03 17:02:02 +0800

    Layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main>
+      <h1 className='text-4xl font-bold'>About</h1>
+      <div className='mt-5'>This is the about page.</div>
+    </main>
+  );
+}
